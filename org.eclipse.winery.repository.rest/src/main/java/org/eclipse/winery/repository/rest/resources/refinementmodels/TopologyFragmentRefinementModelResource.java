d773a8fbddea3786f5d1e0e5f59b80c2ce76d0ea
Björn Müller <bjoern.mueller@kanu-baerchen.de>
2020-12-11 14:45:15 +0100

    remove import Signed-off-by: Björn Müller <bjoern.mueller@kanu-baerchen.de>

--- org.eclipse.winery.repository.rest/src/main/java/org/eclipse/winery/repository/rest/resources/refinementmodels/TopologyFragmentRefinementModelResource.java
+++ org.eclipse.winery.repository.rest/src/main/java/org/eclipse/winery/repository/rest/resources/refinementmodels/TopologyFragmentRefinementModelResource.java
@@ -26,7 +26,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.winery.common.ids.definitions.DefinitionsChildId;
 import org.eclipse.winery.model.adaptation.substitution.refinement.PermutationGenerator;
